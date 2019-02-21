87723befbaa5978f4ca0e59f320a13808359fa5e
Alex Covert <acovert@ucsc.edu>
2019-02-21 14:22:58 -0800

    wandering ai and raycasting sight

---
+++ YouAreTheMonster/Assets/Scripts/LineOfSight.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class LineOfSight : MonoBehaviour
+{
+    [SerializeField]
+    private int sightDistance;
+
+    private RaycastHit hit;
+    private void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out hit, sightDistance))
+        {
+            if(hit.transform.tag == "Player")
+            {
+                Debug.Log("Sees Player");
+            }
+        }
+    }
+
+    private void DrawRay()
+    {
+
+    }
+}
+
